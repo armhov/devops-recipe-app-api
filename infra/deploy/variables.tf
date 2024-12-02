3b9b122157b67c096b49850268755a1a1d1757ee
Armen Hovhannisyan <armen.hov24@gmail.com>
2024-12-02 13:27:46 +0400

    Added terraform

---
+++ infra/deploy/variables.tf
@@ -0,0 +1,15 @@
+variable "prefix" {
+  description = "Prefix for resource in AWS"
+  default     = "raa"
+}
+
+variable "project" {
+  description = "Project name for tagging resources"
+  default     = "recipe-app-api"
+}
+
+
+variable "contact" {
+  description = "Contact email for tagging resources"
+  default     = "armen.hov24@gmail.com"
+}
